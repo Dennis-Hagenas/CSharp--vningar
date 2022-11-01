993b9de95efc3427deffca5c3e530d66c9d7c080
Dennis Hagenäs <dh_lexicon@outlook.com>
2022-11-01 14:15:11 +0100

    Add project files.

---
+++ CSharp-Övningar/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp_Övningar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
