a7064572335bfd52231c654eac6502a91e5d019c
Dennis Hagenäs <dh_lexicon@outlook.com>
2022-11-10 09:35:09 +0100

    Finish PersonHandler

--- Ovning3/PersonHandler.cs
+++ Ovning3/PersonHandler.cs
@@ -6,19 +6,38 @@ using System.Threading.Tasks;
 
 namespace Ovning3
 {
+
     public class PersonHandler
     {
         public void setAge(Person person, int age)
         {
             person.Age = age;
         }
+        public void setFName(Person person, string fName)
+        {
+            person.FName = fName;
+        }
+        public void setLName(Person person, string lName)
+        {
+            person.LName = lName;
+        }
+
+        public void setHeight(Person person, double height)
+        {
+            person.Height = height;
+        }
+
+        public void setWeight(Person person, double weight)
+        {
+            person.Weight = weight;
+        }
 
         public Person CreatePerson(int age, string fName, string lName, double height, double weight)
         {
             Person p = new Person(fName, lName);
             setAge(p, age);
-            p.Height = height;
-            p.Weight = weight;
+            setHeight(p, height);
+            setWeight(p, weight);
             return p;
 
         }
